da80c4c82f7b9d177c972cf879d1dd17bc5119d6
Dmitry Davydov <haudvd@gmail.com>
2013-08-08 15:43:14 +0400

    remove unneeded files

--- start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-erl -config app.config -pa ebin deps/*/ebin -s vis_request \
-    -eval "io:format(\"Point your browser at http://localhost:8080/ to use a simple websocket client~n\")."
-
